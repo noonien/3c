6606d4508d9f94f911301bbcd8ae49f8b79bc6dc
George-Cristian Jiglau <george@mux.ro>
2022-01-04 01:18:19 +0200

    fix cors bridge

--- src/lib/external-data.js
+++ src/lib/external-data.js
@@ -2,9 +2,13 @@ import { cached } from '$lib/local-cache.js';
 
 const CACHE_TOKEN = 1; // used for invalidating old data on new release
 
+function corsBridge(url) {
+    return `https://immense-tundra-51381.herokuapp.com/?url=${encodeURI(url)}`
+}
+
 export function rules() {
     return cached('3c.cache.rules', 24 * 60 * 60, CACHE_TOKEN, () => {
-        return fetch('https://cors.bridged.cc/https://www.binance.com/en/futures/trading-rules')
+        return fetch(corsBridge('https://www.binance.com/en/futures/trading-rules'))
             .then((r) => r.text())
             .then((body) => {
                 const doc = document.createElement('html');
@@ -22,7 +26,7 @@ export function rules() {
 
 export function brackets() {
     return cached('3c.cache.brackets', 24 * 60 * 60, CACHE_TOKEN, () => {
-        return fetch(`https://cors.bridged.cc/https://www.binance.com/bapi/futures/v1/friendly/future/common/brackets`, {
+        return fetch(corsBridge('https://www.binance.com/bapi/futures/v1/friendly/future/common/brackets'), {
             headers: {
                 'content-type': 'application/json'
             },
